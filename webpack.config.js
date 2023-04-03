86f6c01383c6e77ac4d12f316cb5e36d3f56b95b
Anuj Singla <asingla@redhat.com>
2023-04-03 07:26:10 +0530

    code changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    port: 3000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Patternfly elements Project",
-      template: "src/custom.html",
-    }),
-  ],
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        port: 3000,
+        watchFiles: ['./src/**/*.html', './src/**/*.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Patternfly elements Project',
+            template: 'src/custom.html',
+            inject: 'body',
+        }),
+    ],
 };
